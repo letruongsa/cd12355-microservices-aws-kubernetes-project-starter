467cb69eca1e36d5251683847df01ff048e9dbc6
Sa Le <sa.le.ym@renensas.com>
2024-07-06 10:07:56 +0700

    To correct password handling

--- analytics/config.py
+++ analytics/config.py
@@ -5,7 +5,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 db_username = os.environ["DB_USERNAME"]
-db_password = os.environ["DB_PASSWORD"]
+db_password = os.environ["DB_PASSWORD"].rstrip()
 db_host = os.environ.get("DB_HOST", "127.0.0.1")
 db_port = os.environ.get("DB_PORT", "5432")
 db_name = os.environ.get("DB_NAME", "postgres")
